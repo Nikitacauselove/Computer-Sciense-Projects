430a099288cd4b53506ec26431a02e10f367ec50
Козлов Никита <63429080+Nikitacauselove@users.noreply.github.com>
2020-06-18 11:42:29 +0300

    Add files via upload

---
+++ Text Analysis with Threads/Text Analysis with Threads/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Text_Analysis_with_Threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartAnalysis = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.открытьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStartAnalysis
+            // 
+            this.buttonStartAnalysis.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonStartAnalysis.Location = new System.Drawing.Point(13, 132);
+            this.buttonStartAnalysis.Name = "buttonStartAnalysis";
+            this.buttonStartAnalysis.Size = new System.Drawing.Size(154, 23);
+            this.buttonStartAnalysis.TabIndex = 0;
+            this.buttonStartAnalysis.Text = "Начать анализ текста";
+            this.buttonStartAnalysis.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.buttonStartAnalysis.UseVisualStyleBackColor = true;
+            this.buttonStartAnalysis.Click += new System.EventHandler(this.buttonStartAnalysis_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьФайлToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // открытьФайлToolStripMenuItem
+            // 
+            this.открытьФайлToolStripMenuItem.Name = "открытьФайлToolStripMenuItem";
+            this.открытьФайлToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.открытьФайлToolStripMenuItem.Text = "Открыть файл";
+            this.открытьФайлToolStripMenuItem.Click += new System.EventHandler(this.открытьФайлToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(13, 161);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ReadOnly = true;
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(759, 288);
+            this.textBoxOutput.TabIndex = 2;
+            this.textBoxOutput.Visible = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(250, 280);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(300, 23);
+            this.progressBar.TabIndex = 3;
+            this.progressBar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.buttonStartAnalysis);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Анализ текста";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartAnalysis;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem открытьФайлToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ Text Analysis with Threads/Text Analysis with Threads/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+
+
+namespace Text_Analysis_with_Threads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filePath = string.Empty;
+
+        private void открытьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = ".txt|*.txt";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+            }
+        }
+
+        private void buttonStartAnalysis_Click(object sender, EventArgs e)
+        {
+            buttonStartAnalysis.Enabled = false;
+            textBoxOutput.Visible = false;
+            textBoxOutput.Clear();
+
+            if (filePath != string.Empty)
+            {
+                progressBar.Style = ProgressBarStyle.Marquee;
+                progressBar.Visible = true;
+                var analysingThread = new Thread(analysis);
+                analysingThread.Start();
+            }  
+            else
+                MessageBox.Show("Пожалуйста, выберите файл", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            buttonStartAnalysis.Enabled = true;
+        }
+
+
+
+
+        private void analysis()
+        {
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+            string formattedText;
+            string[] lineContent;
+            char[] separators = {' ', '.', ',', '!', '?', ';', ':', '-', '(', ')', '{', '}', '[', ']'};
+
+            using (StreamReader SR = new StreamReader(filePath))
+            {
+                while (!SR.EndOfStream)
+                {
+                    formattedText = SR.ReadLine().ToLower();
+                    lineContent = formattedText.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int i = 0; i < lineContent.Length; i++)
+                        if (wordFrequency.ContainsKey(lineContent[i]))
+                            wordFrequency[lineContent[i]]++;
+                        else
+                            wordFrequency.Add(lineContent[i], 1);
+                }
+            }
+            output(wordFrequency);
+        }
+
+        private void output(Dictionary<string, int> wordFrequency)
+        {
+            ICollection<string> keys = wordFrequency.Keys;
+            if (keys.Count == 0)
+                this.textBoxOutput.BeginInvoke((MethodInvoker)(() => this.textBoxOutput.AppendText("В тексте нет слов" + Environment.NewLine)));
+            else
+                foreach (string key in keys)
+                {
+                    this.textBoxOutput.BeginInvoke((MethodInvoker)(() => this.textBoxOutput.AppendText("Слово " + key + " встречается " + wordFrequency[key] + " раз" + Environment.NewLine)));              
+                }
+
+            Thread.Sleep(1000);
+
+            this.progressBar.BeginInvoke((MethodInvoker)(() => this.progressBar.Visible = false));
+            this.progressBar.BeginInvoke((MethodInvoker)(() => this.progressBar.Style = ProgressBarStyle.Blocks));
+            this.textBoxOutput.BeginInvoke((MethodInvoker)(() => this.textBoxOutput.Visible = true));
+        }
+    }
+}
